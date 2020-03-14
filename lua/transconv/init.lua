4322438a2a06a8b89f0abf410ba5d2995c8d5c3e
Abun <erandur@web.de>
2020-03-14 15:17:00 +0800

    First attemt at implementation
    Currently buggy. Causes incorrect setup of schemes, even if the function
    is not called itself

--- lua/transconv/init.lua
+++ lua/transconv/init.lua
@@ -19,6 +19,58 @@ local function path_of(path)
     return path:match("^(.-)%.[^%.]+$")
 end
 
+-- TODO
+--local function make_default_scheme(lang, scheme)
+--    --[[
+--        Moves scheme to the front of lang's list in default_languages. Returns
+--        integer code to reflect what was done:
+--            1: scheme moved to front
+--            0: scheme already default for lang (no changes done)
+--           -1: scheme currently does not exist for lang (error)
+--           -2: there is no scheme set up for lang at all (error)
+--    --]]
+
+--    local function move_element_to_front(list, old_i)
+--        if old_i <= 1 then
+--            return table
+--        end
+
+--        local l = {[1] = list[old_i]}
+
+--        for k,v in pairs(list) do
+--            if k ~= old_i then
+--                table.insert(l, v)
+--            end
+--        end
+
+--        return l
+--    end
+
+--    -- return error code if no scheme is set up for lang
+--    if default_schemes["lang"] == nil do
+--        return -2
+--    end
+
+--    -- find scheme in default_schemes list for lang and get its index
+--    local index = 0
+--    for k,v in pairs(default_schemes.[lang]) do
+--        if v == scheme then
+--            index = k
+--            break
+--        end
+--    end
+
+--    -- handle depending on if the scheme was found and at what index
+--    if index == 0 then
+--        -- index 0 means scheme was not found because Lua starts indexing at 1
+--        return -1
+--    elseif index == 1 then
+--        return 0
+--    else
+--        default_schemes[lang] = move_element_to_front(default_schemes[lang], index)
+--    end
+--end
+
 local function new_converter(self, first, second, third)
     Converter = require(main_dir.."converter")
     Raw = require(main_dir.."raw")
@@ -74,6 +126,7 @@ transconv = {
     main_dir = main_dir,
     -- functions
     path_of = path_of,
+    -- make_default_scheme = make_default_scheme,
     new_converter = new_converter,
     -- uselangs = uselangs,
     schemes = schemes,
