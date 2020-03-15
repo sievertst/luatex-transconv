81e73dc777f6cade5ead7be111d9f99f162998c7
Abun <erandur@web.de>
2020-03-15 14:22:39 +0800

    Implementing make_default_scheme, also in sty file
    Currently buggy: both \tolang and \toscheme always seem to use the
    original default scheme (i.e. the first one to be imported for a
    language)

--- lua/transconv/init.lua
+++ lua/transconv/init.lua
@@ -19,57 +19,56 @@ local function path_of(path)
     return path:match("^(.-)%.[^%.]+$")
 end
 
--- TODO
---local function make_default_scheme(lang, scheme)
---    --[[
---        Moves scheme to the front of lang's list in default_languages. Returns
---        integer code to reflect what was done:
---            1: scheme moved to front
---            0: scheme already default for lang (no changes done)
---           -1: scheme currently does not exist for lang (error)
---           -2: there is no scheme set up for lang at all (error)
---    --]]
+local function make_default_scheme(self, lang, scheme)
+    --[[
+        Moves scheme to the front of lang's list in default_languages. Returns
+        integer code to reflect what was done:
+            1: scheme moved to front
+            0: scheme already default for lang (no changes done)
+           -1: scheme currently does not exist for lang (error)
+           -2: there is no scheme set up for lang at all (error)
+    --]]
 
---    local function move_element_to_front(list, old_i)
---        if old_i <= 1 then
---            return table
---        end
+    local function move_element_to_front(list, old_i)
+        if old_i <= 1 then
+            return table
+        end
 
---        local l = {[1] = list[old_i]}
+        local l = {[1] = list[old_i]}
 
---        for k,v in pairs(list) do
---            if k ~= old_i then
---                table.insert(l, v)
---            end
---        end
+        for k,v in pairs(list) do
+            if k ~= old_i then
+                table.insert(l, v)
+            end
+        end
 
---        return l
---    end
+        return l
+    end
 
---    -- return error code if no scheme is set up for lang
---    if default_schemes["lang"] == nil do
---        return -2
---    end
+    -- return error code if no scheme is set up for lang
+    if self.default_schemes[lang] == nil then
+        return -2
+    end
 
---    -- find scheme in default_schemes list for lang and get its index
---    local index = 0
---    for k,v in pairs(default_schemes.[lang]) do
---        if v == scheme then
---            index = k
---            break
---        end
---    end
+    -- find scheme in default_schemes list for lang and get its index
+    local index = 0
+    for k,v in pairs(self.default_schemes[lang]) do
+        if v == scheme then
+            index = k
+            break
+        end
+    end
 
---    -- handle depending on if the scheme was found and at what index
---    if index == 0 then
---        -- index 0 means scheme was not found because Lua starts indexing at 1
---        return -1
---    elseif index == 1 then
---        return 0
---    else
---        default_schemes[lang] = move_element_to_front(default_schemes[lang], index)
---    end
---end
+    -- handle depending on if the scheme was found and at what index
+    if index == 0 then
+        -- index 0 means scheme was not found because Lua starts indexing at 1
+        return -1
+    elseif index == 1 then
+        return 0
+    else
+        self.default_schemes[lang] = move_element_to_front(self.default_schemes[lang], index)
+    end
+end
 
 local function new_converter(self, first, second, third)
     Converter = require(main_dir.."converter")
@@ -102,31 +101,22 @@ local function new_converter(self, first, second, third)
         lang_module = lang
     end
 
-    local c = require(string.format("%s.%s", lang_module, scheme))
+    tex.sprint(string.format("Setting up converter: %s.%s", lang,scheme))
 
-    self.default_schemes[lang] = self.default_schemes[lang] or c
+    local c = require(string.format("%s.%s", lang_module, scheme)):new(options)
 
-    return c:new(options)
+    self.schemes[scheme] = c
+    self.default_schemes[lang] = self.default_schemes[lang] or {}
+    table.insert(self.default_schemes[lang], c)
+
+    return c
 end
-
--- function uselangs(str)
---     for l in str:gmatch("[^,]*") do
---         local fields = {}
---         l:gsub("(%w*)", function(a) table.insert(fields, a) end)
---         transconv[fields[2]] = transconv.new_converter(fields[1], fields[2])
-
---         tex.sprint("\\newcommand{\\"..fields[2].."convert}[1]{%")
---         tex.sprint("\\directlua{")
---         tex.sprint("transconv["..fields[2].."]:convert('#1'))}%")
---         tex.sprint("}")
---     end
--- end
 
 transconv = {
     main_dir = main_dir,
     -- functions
     path_of = path_of,
-    -- make_default_scheme = make_default_scheme,
+    make_default_scheme = make_default_scheme,
     new_converter = new_converter,
     -- uselangs = uselangs,
     schemes = schemes,
