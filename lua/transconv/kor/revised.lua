69f42832bcbc995a6f765202880b217e8a4aa7c5
sievertst <sievert.stuelpnagel@mail.de>
2020-03-25 17:09:48 +0800

    Finish first version of kor.revised

--- lua/transconv/kor/revised.lua
+++ lua/transconv/kor/revised.lua
@@ -1,7 +1,9 @@
 #!/usr/bin/env lua5.3
 
 local function to_target_scheme(self, instring)
-    return self.do_str_rep(self, instring, self.rep_strings)
+    -- insert ' at the end of the string, so the end of a string-final syllable
+    -- doesn't have to be a separate case. The ' is deleted during replacements
+    return self.do_str_rep(self, instring.."\'", self.rep_strings)
 end
 
 local Revised = Converter:new{
@@ -12,69 +14,75 @@ local Revised = Converter:new{
         -- level out unsupported vowel length and arae-a
         {"=", ""}, {"v", "a"},
 
-        -- distinguish word-internal initial i-eung both from other consonants
-        -- and from morpheme breaks with following consonant
-        {"[\'-]a", "-a"}, {"[\'-]e", "-e"}, {"[\'-]i", "-i"},
-        {"[\'-]o", "-o"}, {"[\'-]u", "-u"}, {"[\'-]y", "-y"},
-        {"[\'-]w", "-w"},
+        -- insert ' at the end of word-final syllables for easier processing
+        {" ", "\' "}, {"%.", "\'."}, {"%?", "\'?"}, {"!", "\'!"},
+        {")", "\')"}, {"%-", "\'-"},
+
+        -- always use r for initial rieul
+        {"la", "ra"}, {"le", "re"}, {"li", "ri"}, {"lo", "ro"}, {"lu", "ru"},
+        -- always use
+
+        -- insert marker at the end of syllables which are followed by
+        -- vowel-initial syllables
+        {"\'a", "x\'a"}, {"\'e", "x\'e"}, {"\'i", "x\'i"},
+        {"\'o", "x\'o"}, {"\'u", "x\'u"}, {"\'y", "x\'y"},
+        {"\'w", "x\'w"},
+        {"\'%-a", "x\'-a"}, {"\'%-e", "x\'-e"}, {"\'%-i", "x\'-i"},
+        {"\'%-o", "x\'-o"}, {"\'%-u", "x\'-u"}, {"\'%-y", "x\'-y"},
+        {"\'%-w", "x\'-w"},
+
 
         {"ng", "ŋ"}, -- for easier processing
 
-        -- g
-        {"g\'g", "kg"}, {"[ŋgk]\'[nlr]", "ngn"}, {"g\'d", "kd"},
-        {"[gk]\'m", "ngm"}, {"g\'b", "kb"}, {"g\'s", "ks"}, {"g\'j", "kj"}, {"g\'c", "kc"},
-        {"g\'k", "k-k"}, {"g\'t", "kt"}, {"g\'p", "kp"}, {"g\'h", "kh"},
-        -- n
-        {"n\'g", "n-g"}, {"n\'[lr]", "ll"},
-        -- d
-        {"[dsj]\'g", "tg"}, {"[dtsjh]\'[nlr]", "nn"}, {"[dsj]\'d", "td"},
-        {"[dtsj]\'m", "nm"}, {"[dsj]\'b", "tb"}, {"[dsj]\'s", "ts"},
-        {"[dsj]\'j", "tj"}, {"[dsjh]\'c", "tc"},
-        {"[dsjh]\'k", "tk"}, {"[dsj]\'t", "t-t"}, {"[dsjh]\'p", "tp"}, {"[dsj]\'h", "th"},
-        -- r/l
-        {"r\'g", "lg"}, {"[lr]\'[nlr]", "ll"}, {"r\'d", "ld"},
-        {"r\'m", "lm"}, {"r\'b", "lb"}, {"r\'s", "ls"}, {"r\'j", "lj"}, {"r\'c", "lc"},
-        {"r\'k", "lk"}, {"r\'t", "lt"}, {"r\'p", "lp"}, {"r\'h", "lh"},
-        -- m
-        {"[bpm]\'[nlr]", "mn"},
-        -- b
-        {"b\'g", "pg"}, {"b\'d", "td"},
-        {"[bp]\'m", "mm"}, {"b\'b", "pb"}, {"b\'s", "ps"}, {"b\'j", "pj"}, {"b\'c", "pc"},
-        {"b\'k", "pk"}, {"b\'t", "pt"}, {"b\'p", "p-p"}, {"b\'h", "ph"},
-        -- h
-        {"h\'g", "k"}, {"h\'d", "t"}, {"h\'b", "p"}, {"h\'h", "t"},
+        -- hieut-assimilations (including nh and lh)
+        {"g\'h", "k\'h"}, {"h\'g", "\'k"}, {"h\'k", "\'k"},
+        {"d\'h", "t\'h"}, {"h\'d", "\'t"}, {"h\'t", "\'t"},
+        {"b\'h", "p\'h"}, {"h\'b", "\'p"}, {"h\'p", "\'p"},
+        {"j\'h", "ch\'"}, {"h\'j", "\'ch"}, {"h\'ch", "\'ch"},
+        {"g\'%-h", "k\'-h"}, {"h\'%-g", "\'-k"}, {"h\'%-k", "\'-k"},
+        {"d\'%-h", "t\'-h"}, {"h\'%-d", "\'-t"}, {"h\'%-t", "\'-t"},
+        {"b\'%-h", "p\'-h"}, {"h\'%-b", "\'-p"}, {"h\'%-p", "\'-p"},
+        {"j\'%-h", "ch\'-"}, {"h\'%-j", "\'-ch"}, {"h\'%-ch", "\'-ch"},
 
-        -- TODO: palatalisation of digeut and ti-eut
-
-        -- use pronunciation of finals but preserve before vowels
-        -- {"g\'", "k"}, {"d\'", "t\'"}, {"b\'", "p\'"}, {"j\'", "t\'"},
-
-        {"kk\'", "k\'"}, {"tt\'", "t\'"}, {"pp\'", "p\'"}, {"ss\'", "t\'"},
-        {"r\'", "l\'"}, {"s\'", "t\'"}, {"jj\'", "t\'"}, {"ch\'", "t\'"},
-        -- hi-eut
-        {"h\'g", "k\'"}, {"h\'d", "t\'"}, {"h\'b", "p\'"}, {"h\'", "t\'"},
-
+        -- syllable-final (not before vowel)
+        {"gg\'", "k\'"}, {"kk\'", "k\'"}, {"g\'", "k\'"},
+        {"dd\'", "t\'"}, {"ss\'", "t\'"}, {"jj\'", "t\'"}, {"tt\'", "t\'"},
+        {"d\'", "t\'"}, {"s\'", "t\'"}, {"j\'", "t\'"}, {"t\'", "t\'"},
+        {"ch\'", "t\'"},
+        {"r\'", "l\'"},
+        {"bb\'", "p\'"}, {"pp\'", "p\'"}, {"b\'", "p\'"},
+        {"h\'", "t\'"},
         -- double consonants
         {"gs\'", "k\'"}, {"rg\'", "k\'"}, {"lg\'", "k\'"},
-        {"nj\'", "n\'"}, {"nh\'", "n\'"},
-        {"lb\'", "l\'"}, {"ls\'", "l\'"}, {"lt\'", "l\'"}, {"lh\'", "l\'"},
+        {"nj\'", "n\'"},
+        {"lb\'", "l\'"}, {"ls\'", "l\'"}, {"lt\'", "l\'"},
         {"bs\'", "p\'"}, {"lp\'", "p\'"},
         {"lm\'", "m\'"},
-        -- TODO: only preserve consonant before morpheme break if vowel follows
 
-        -- delete deviding apostrophe
-        {"\'", ""},
+        -- syllable-finals (before vowel)
+        {"gsx", "ksx"}, {"rg", "lgx"}, {"nhx", "nx"},
+        {"lhx", "rx"}, {"rhx", "rx"},
+        {"cx", "chx"}, -- repair final ch before vowels
+
+        -- TODO: palatalisation of digeut and ti-eut
+        {"dx\'i", "jx\'i"}, {"dx\'%-i", "jx\'-i"},
+        {"tx\'i", "chx\'i"}, {"tx\'%-i", "chx\'-i"},
+
+        -- get rid of helping characters
+        {"ŋ", "ng"},
+        -- insert - before syllable-initial vowels (but pay attention not to
+        -- produce any double hyphens -- or delete any that the user entered)
+        {"x\'", "-x"}, {"%-x%-", "-"},
+        {"x", ""}, {"\'", ""},
 
         -- assimilations
-        {"kn", "ngn"}, {"kr", "ngn"}, {"km", "ngm"}, {"kh", "kh"},
-        {"tn", "nn"}, {"tr", "nn"}, {"tm", "nm"}, {"th", "th"},
-        {"pn", "mn"}, {"pr", "mn"}, {"pm", "mm"}, {"ph", "ph"},
+        {"kn", "ngn"}, {"kr", "ngn"}, {"km", "ngm"},
+        {"tn", "nn"}, {"tr", "nn"}, {"tm", "nm"},
+        {"pn", "mn"}, {"pr", "mn"}, {"pm", "mm"},
         {"lr", "ll"}, {"ln", "ll"}, {"nl", "ll"},
-
     },
 
-    -- functions
-    -- do_str_rep = do_str_rep,
+    -- -- functions
     to_target_scheme = to_target_scheme,
     convert = convert,
 }
