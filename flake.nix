5e7bb4a536103d508dbf37203d9de0206f02653c
sievertst <sievert.stuelpnagel@mail.de>
2024-07-26 17:03:17 +0200

    Use flake-parts instead of flake-utils
    Also add formatter

--- flake.nix
+++ flake.nix
@@ -3,55 +3,69 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixfmt.url = "github:NixOS/nixfmt";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let 
-        pkgs = nixpkgs.legacyPackages.${system};
-        pname = "transconv";
-        outPack = pkgs.stdenvNoCC.mkDerivation {
-          inherit pname;
-          version = "1.0";
-          outputs = [ "tex" ];
-          src = self;
-          # stop nix from complaining about missing output "out"
-          nativeBuildInputs = [
-            (pkgs.writeShellScript "force-tex-output.sh" ''
-              out="''${tex-}"
-            '')
-          ];
-          # buildPhase would run the makefile which is only intended
-          # for people without nix
-          dontBuild = true;
-          installPhase = ''
-            runHook preInstall
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-            texpath="$tex/tex/latex/${pname}"
-            luapath="$tex/tex/latex/${pname}"
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pname = "transconv";
+          outPack = pkgs.stdenvNoCC.mkDerivation {
+            inherit pname;
+            version = "1.0";
+            outputs = [ "tex" ];
+            src = self;
+            # stop nix from complaining about missing output "out"
+            nativeBuildInputs = [
+              (pkgs.writeShellScript "force-tex-output.sh" ''
+                out="''${tex-}"
+              '')
+            ];
+            # buildPhase would run the makefile which is only intended
+            # for people without nix
+            dontBuild = true;
+            installPhase = ''
+              runHook preInstall
 
-            mkdir -p "$texpath"
-            cp *.{sty,cls,dev,clo} "$texpath"
+              texpath="$tex/tex/latex/${pname}"
+              luapath="$tex/tex/latex/${pname}"
 
-            luapath="$tex/scripts/${pname}"
-            mkdir -p "$luapath"
-            cp -r lua/${pname}/* "$luapath"
-            # the main file should be named like the package in this
-            # location, not init
-            mv "$luapath/init.lua" "$luapath/${pname}.lua"
+              mkdir -p "$texpath"
+              cp *.{sty,cls,dev,clo} "$texpath"
 
-            runHook postInstall
-          '';
+              luapath="$tex/scripts/${pname}"
+              mkdir -p "$luapath"
+              cp -r lua/${pname}/* "$luapath"
+              # the main file should be named like the package in this
+              # location, not init
+              mv "$luapath/init.lua" "$luapath/${pname}.lua"
+
+              runHook postInstall
+            '';
+          };
+          # TODO
+          documentation = { };
+        in
+        {
+          packages = {
+            package = outPack;
+            doc = documentation;
+            default = outPack;
+          };
+
+          formatter = inputs.nixfmt.packages.${system}.default;
         };
-        # TODO
-        documentation = {};
-      in
-      {
-        packages = {
-          package = outPack;
-          doc = documentation;
-          default = outPack;
-        };
-      });
+    };
 }
